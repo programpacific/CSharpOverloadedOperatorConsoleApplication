2cce29aa594e560f8559ca45f6395df9e345d60d
programpacific <programpacific@users.noreply.github.com>
2018-08-01 15:31:01 -0600

    Initial commit.

---
+++ CSharpOverloadedOperatorConsoleApplication/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharpOverloadedOperatorConsoleApplication
+{
+    class Employee
+    {
+        //EXERCISE:
+        //IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+        //1. Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+
+
+        public int Id { get; set; }
+
+
+        public static bool operator ==(Employee employee, Employee employeeTwo)
+        {
+            if (employee.Id == employeeTwo.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Employee employee, Employee employeeTwo)
+        {
+            if (employee.Id == employeeTwo.Id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
--- CSharpOverloadedOperatorConsoleApplication/Program.cs
+++ CSharpOverloadedOperatorConsoleApplication/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpOverloadedOperatorConsoleApplication
 {
     class Program
     {
+        //EXERCISE:
+        //IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+        //1. Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+
         static void Main(string[] args)
         {
+            Employee employee = new Employee() { Id = 123 };
+            Employee employeeTwo = new Employee() { Id = 456 };
+
+            Console.WriteLine(Convert.ToString(employee == employeeTwo));
+            Console.ReadLine();
         }
+
     }
 }
